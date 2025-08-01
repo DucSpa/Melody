92843233fdbbfd81c15e45231329165c7c113e7b
DucSpa <ducspa1823@gmail.com>
2025-08-01 19:46:59 +0200

    now downloadsong.js uploads an mp3

--- src/slashCommands/downloadsong.js
+++ src/slashCommands/downloadsong.js
@@ -1,34 +1,38 @@
+// download.js
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const ytdl = require("@distube/ytdl-core");
-const {getInfo} = require("@distube/ytdl-core");
+const { getInfo } = require("@distube/ytdl-core");
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("download")
         .setDescription("Provides a link to download the song you want!")
-        .setIntegrationTypes(0, 1)
-        .setContexts(0, 1, 2)
-        .addStringOption((option) =>
+        .addStringOption(option =>
             option
                 .setName("link")
-                .setDescription("Write the URL or name of the song to download! :3")
-                .setRequired(true),
+                .setDescription("Write the URL of the song to download!")
+                .setRequired(true)
         ),
+
     async execute(interaction) {
         const songLink = interaction.options.getString("link");
 
+        // Validate input
         if (!songLink) {
             return interaction.reply({
                 embeds: [
                     new EmbedBuilder()
-                        .setDescription("Please provide a valid song link or name.")
+                        .setDescription("Please provide a valid song link.")
                         .setColor("#fdc2a2"),
                 ],
                 ephemeral: true,
             });
         }
 
-        if(!ytdl.validateURL(songLink)) {
+        if (!ytdl.validateURL(songLink)) {
             return interaction.reply({
                 embeds: [
                     new EmbedBuilder()
@@ -39,40 +43,61 @@ module.exports = {
             });
         }
 
-        const songInfo = await getInfo(songLink);
-        const song= ytdl.downloadFromInfo(songInfo, { filter: "audioonly", quality: "highestaudio", language: "en" });
-        const songTitle = songInfo.videoDetails.title;
-        const songUrl = songInfo.videoDetails.video_url;
-        const songThumbnail = songInfo.videoDetails.thumbnails[0].url;
-        const user = interaction.user;
-        const userImage = user.displayAvatarURL();
-
         try {
-            await interaction.deferReply() // Defer reply to handle potential long response times
+            await interaction.deferReply();
+
+            // Fetch video info and source stream
+            const songInfo = await getInfo(songLink);
+            const sourceStream = ytdl.downloadFromInfo(songInfo, {
+                filter: "audioonly",
+                quality: "highestaudio",
+            });
+
+            // Transcode to MP3 via FFmpeg
+            const mp3Stream = ffmpeg(sourceStream)
+                .format("mp3")
+                .audioBitrate(192)
+                .on("error", err => {
+                    console.error("FFmpeg error:", err);
+                })
+                .pipe();
+
+            // Collect MP3 data
             const chunks = [];
-            for await (const chunk of song) {
+            for await (const chunk of mp3Stream) {
                 chunks.push(chunk);
             }
-            const fileBuffer = Buffer.concat(chunks);
+            const mp3Buffer = Buffer.concat(chunks);
+
+            // Prepare embed
+            const songTitle = songInfo.videoDetails.title;
+            const songUrl = songInfo.videoDetails.video_url;
+            const songThumbnail = songInfo.videoDetails.thumbnails[0].url;
+            const user = interaction.user;
+
+            // Send the MP3 file
             await interaction.editReply({
                 embeds: [
                     new EmbedBuilder()
-                        .setDescription(`**[${songTitle}](${songUrl})** \n has been downloaded!`)
+                        .setDescription(`**[${songTitle}](${songUrl})** has been downloaded!`)
                         .setThumbnail(songThumbnail)
-                        .setFooter({ text: `Requested by ${user.username}`, iconURL: userImage })
+                        .setFooter({ text: `Requested by ${user.username}`, iconURL: user.displayAvatarURL() })
                         .setColor("#fdc2a2"),
                 ],
-                files: [{
-                    attachment: fileBuffer,
-                    name: `${songTitle}.mp3`
-                }]
+                files: [
+                    {
+                        attachment: mp3Buffer,
+                        name: `${songTitle}.mp3`,
+                    },
+                ],
             });
+
         } catch (error) {
             console.error(error);
             if (interaction.replied || interaction.deferred) {
-                await interaction.followUp("There was an error trying to download the song.");
+                await interaction.followUp({ content: "There was an error trying to download the song.", ephemeral: true });
             } else {
-                await interaction.reply("There was an error trying to download the song.");
+                await interaction.reply({ content: "There was an error trying to download the song.", ephemeral: true });
             }
         }
     },
